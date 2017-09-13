f6616ccf052c487efdc8930e50cb573cecd7c535
Oleksii Novikov <oleksii.novikov@gmail.com>
2017-09-13 15:41:49 +0300

    Add progress bar

--- resources/assets/js/components/quotes/Quotes.vue
+++ resources/assets/js/components/quotes/Quotes.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="container">
+        <quotes-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></quotes-header>
         <quotes-form @quoteAdded="newQuote"></quotes-form>
         <quotes-grid :quotes="quotes" @quoteDeleted="deleteQuote"></quotes-grid>
-        <div class="col-sm-12 text-center">
-            <div class="alert alert-info"> Info: Click on Quote to delete it!</div>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info"> Info: Click on Quote to delete it!</div>
+            </div>
         </div>
     </div>
 </template>
 <script>
     import QuotesGrid from './QuotesGrid.vue';
     import QuotesForm from './Form.vue';
+    import QuotesHeader from './QuoteHeader.vue';
 
     export default {
         data: () => ({
@@ -20,6 +24,9 @@
         }),
         methods: {
             newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert("You have to delete one item to add more");
+                }
                 this.quotes.push(quote);
             },
             deleteQuote(index) {
@@ -28,7 +35,8 @@
         },
         components: {
             QuotesGrid,
-            QuotesForm
+            QuotesForm,
+            QuotesHeader
         }
     }
 </script>
